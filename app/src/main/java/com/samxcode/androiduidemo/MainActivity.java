92c78885a7c755bbe95abb7fa7bddde649c3e8b4
SamXCode <samxcode@hotmail.com>
2016-08-29 01:57:54 +0800

    add android event demo

--- app/src/main/java/com/samxcode/androiduidemo/MainActivity.java
+++ app/src/main/java/com/samxcode/androiduidemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.samxcode.androiduidemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
